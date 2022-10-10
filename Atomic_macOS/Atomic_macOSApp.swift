57942db826fe33c24b72181614c3a42c42538108
Christian Dominguez <christian.99dominguez@gmail.com>
2022-10-10 16:36:00 +0200

    Implemented sparkle framework

--- Atomic_macOS/Atomic_macOSApp.swift
+++ Atomic_macOS/Atomic_macOSApp.swift
@@ -6,9 +6,18 @@
 //
 
 import SwiftUI
+import Sparkle
 
 @main
 struct Atomic_macOSApp: App {
+    
+    private let updaterController: SPUStandardUpdaterController
+    
+    init() {
+        // If you want to start the updater manually, pass false to startingUpdater and call .startUpdater() later
+        // This is where you can also pass an updater delegate if you need one
+        updaterController = SPUStandardUpdaterController(startingUpdater: true, updaterDelegate: nil, userDriverDelegate: nil)
+    }
     
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
     
@@ -19,7 +28,7 @@ struct Atomic_macOSApp: App {
             MainWindow()
         }
         .commands {
-            AtomicCommands()
+            AtomicCommands(updaterController: updaterController)
         }
         Settings {
             SettingsView()
